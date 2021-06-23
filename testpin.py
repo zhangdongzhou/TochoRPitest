b3bd6ed1c194a979601cceafaeda9c91c64c555d
zhangdongzhou <tochozhang@gmail.com>
2021-06-22 23:22:53 -0500

    Add files via upload
    testpin.py

---
+++ testpin.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+import time
+GPIOnum = 38
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(GPIOnum, GPIO.OUT)
+GPIO.output(GPIOnum, GPIO.HIGH)
+time.sleep(3)
+GPIO.output(GPIOnum, GPIO.LOW)
+GPIO.cleanup()
